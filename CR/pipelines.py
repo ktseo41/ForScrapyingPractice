9052577abd7519a71b5683d275849f23fe93d2d7
ktseo41 <ktseo41@naver.com>
2018-02-21 17:48:25 +0900

    last move

--- CR/pipelines.py
+++ CR/pipelines.py
@@ -4,18 +4,34 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+# siteTitle, siteKeywords, title, url, titleTime, replyUrl, replauthor, replcontext, replTime
+import datetime
+import logging
+import pymysql.cursors
+from CR.items import Crypitem
 
 class CrPipeline(object):
-    def __init(self):
-        try:
-            self.conn = MySQLdb.connect(user='root', passwd='tjqhgus11', db='', host='')
-            print ("1")
-            self.cursor = self.conn.cursor()
-        except MySQLdb.error, e:
-            print ("Error %d: %s") % (e.args[0], e.args[1])
-            sys.exit(1)
+    def __init__(self):
+        self.conn = pymysql.connect(user='root', passwd='tjqhgus11', db='cr', host='localhost')
+        self.cursor = self.conn.cursor()
+
 
     def process_item(self, item, spider):
-        self.cursor.execute("select * from cryptodb.cr where")
-        return item
+        """
+        self.cursor.execute("select * from cr.cryptodb where siteTitle = %s and siteKeywords = %s and title = %s and url = %s and titleTime = %s and replyUrl = %s and replauthor = %s and replcontext = %s and replTime = %s", (item['siteTitle'][0].encode('utf-8'), item['siteKeywords'][0].encode('utf-8'), item['title'][0].encode('utf-8'), item['url'][0].encode('utf-8'), item['titleTime'][0].encode('utf-8'), item['replyUrl'][0].encode('utf-8'), item['replauthor'][0].encode('utf-8'), item['replcontext'][0].encode('utf-8'), item['replTime'][0].encode('utf-8')))
+        result = self.cursor.fetchone()
+
+        if result:
+            print("data already exist")
+
+        else:
+            try:"""
+        self.cursor.execute("insert into cr.cryptodb(siteTitle, siteKeywords, title, url, titleTime, replyUrl, replauthor, replcontext, replTime) values (%s %s %s %s %s %s %s %s %s)", (item['siteTitle'][0].encode('utf-8'), item['siteKeywords'][0].encode('utf-8'), item['title'][0].encode('utf-8'), item['url'][0].encode('utf-8'),
+        item['titleTime'][0].encode('utf-8'), item['replyUrl'][0].encode('utf-8'), item['replauthor'][0].encode('utf-8'), item['replcontext'][0].encode('utf-8'), item['replTime'][0].encode('utf-8')))
+        self.conn.commit()
+
+        """
+            except pymysql.Error as e:
+                print ("Error %d: %s") % (e.args[0], e.args[1])
+                return item
+        """
